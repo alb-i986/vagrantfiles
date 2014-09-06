3e79a2abbc2d3bed89c20e842f38957cf7e73e7d
Alberto Scotto <scotto.alberto.86@gmail.com>
2014-09-06 15:03:00 +0100

    improving setup.sh: downloading deb pkgs to /tmp

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 # install prereq
 # (very basic script, for debian x86_64)
 
+PROJECT_NAME=vagrantfiles
+TMP_DIR=$(mktemp -d --tmpdir $PROJECT_NAME.XXXXXX)
 
 is_cmd_available() {
   cmd=$1
@@ -13,15 +15,15 @@ is_cmd_available() {
 # virtualbox
 is_cmd_available virtualbox || {
   VIRTUALBOX_DEB=virtualbox-4.3_4.3.14-95030~Debian~wheezy_amd64.deb
-  wget http://download.virtualbox.org/virtualbox/4.3.14/$VIRTUALBOX_DEB
-  sudo dpkg -i $VIRTUALBOX_DEB
+  wget -O $TMP_DIR/$VIRTUALBOX_DEB http://download.virtualbox.org/virtualbox/4.3.14/$VIRTUALBOX_DEB &&
+  sudo dpkg -i $TMP_DIR/$VIRTUALBOX_DEB
 }
 
 # vagrant
 is_cmd_available vagrant || {
   VAGRANT_DEB=vagrant_1.6.5_x86_64.deb
-  wget https://dl.bintray.com/mitchellh/vagrant/$VAGRANT_DEB
-  sudo dpkg -i $VAGRANT_DEB
+  wget -O $TMP_DIR/$VAGRANT_DEB https://dl.bintray.com/mitchellh/vagrant/$VAGRANT_DEB &&
+  sudo dpkg -i $TMP_DIR/$VAGRANT_DEB
 }
 
 # vagrant plugins
@@ -31,6 +33,7 @@ vagrant plugin install vagrant-librarian-chef
 
 # chef
 ## ?? is it installed by vagrant-omnibus ??
+#https://opscode-omnibus-packages.s3.amazonaws.com/ubuntu/12.04/x86_64/chef-server_11.1.3-1_amd64.deb
 
 # bundler
 sudo gem install bundler
